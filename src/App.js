dfdffae2aa045b635c84c642d3033156587594f1
Ivo Krastev <ivo.krustev@yahoo.com>
2020-03-30 15:14:18 +0300

    files modified

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class AppToDo extends React.Component {
+    constructor(){
+        super()
+        this.state = {
+            toDo: "",
+            item: "",
+            items: []
+        }
+        this.eventHandler = this.eventHandler.bind(this)
+        this.dataItem = this.dataItem.bind(this)
+    }
+
+    eventHandler(evt){
+        const {name, value} = evt.target
+        this.setState({
+            [name]: value
+        })
+    }
+
+    dataItem(){
+        const item = this.state.item;
+        const items = this.state.items;
+        items.push(item)
+        this.setState({
+            items: items
+        })
+    }
+
+    render(){
+        return(
+            <div>
+                <h1>To Do App</h1>
+                <input type="text"
+                name="item"
+                onChange={this.eventHandler}
+                />
+                <button onClick={this.dataItem}>Submit</button>
+                {this.state.items.map((item) => {
+                    return(
+                        <p>{item}</p>
+                    )
+                })}
+            </div>
+        )
+    }
 }
 
-export default App;
+export default AppToDo;
