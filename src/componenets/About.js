12053fe805dc8c95c7bd7b6860bfb2dbac67afff
Ivo Krastev <ivo.krustev@yahoo.com>
2020-04-10 15:56:46 +0300

    files modified

--- src/componenets/About.js
+++ src/componenets/About.js
@@ -1,17 +1,36 @@
 import React from "react";
+import { useSpring, animated } from "react-spring";
 
 function About() {
+  const spring = useSpring({
+    from: { marginTop: -1500 },
+    to: { marginTop: 0 },
+    delay: 1000,
+    config: { duration: 200 },
+  });
+  const springFade = useSpring({
+    opacity: 1,
+    from: { opacity: 0 },
+    config: { duration: 1000 },
+  });
   return (
     <div>
-      <h2 className="hello">Hello there! Thank you for checking my project!</h2>
-      <div className="about-flex">
-        <img className="me" src={require("../assets/me.jpg")} alt="its me" />
-        <p className="info">
-          This is my ToDo App version 2.0 built with React.js, React Router,
-          Class Components, functional Components, State Componenets, Hooks,
-          CSS3, etc...
-        </p>
-      </div>
+      <animated.div style={springFade}>
+        <h2 className="hello">
+          Hello there! Thank you for checking my project!
+        </h2>
+      </animated.div>
+
+      <animated.div style={spring}>
+        <div className="about-flex">
+          <img className="me" src={require("../assets/me.jpg")} alt="its me" />
+          <p className="info">
+            This is my ToDo App version 2.2 built with React.js, React Router,
+            Class Components, functional Components, State Componenets, Hooks,
+            React-Spring, CSS3, etc...
+          </p>
+        </div>
+      </animated.div>
     </div>
   );
 }
--- src/componenets/Footer.js
+++ src/componenets/Footer.js
@@ -2,14 +2,17 @@ import React from "react";
 
 function Footer() {
   return (
-    <div className="footer">
-      <a
-        target="_blank"
-        href="https://github.com/BrascoBG/To-Do-List"
-        rel="noopener noreferrer"
-      >
-        Code here
-      </a>
+    <div>
+      <div className="footer">
+        <a
+          target="_blank"
+          href="https://github.com/BrascoBG/To-Do-List"
+          rel="noopener noreferrer"
+        >
+          Code here
+        </a>
+      </div>
+      <p style={{ padding: "30px" }}></p>
     </div>
   );
 }
